7a1b86577f12599f01b9cace3cb4a6706d9769a5
Reybin <rurena.dev@gmail.com>
2019-06-22 21:20:27 -0700

    adding baseService and basic CRUD for Hotel and RoomType Entities

--- HotelAPI/Data/Context/HotelDbContext.cs
+++ HotelAPI/Data/Context/HotelDbContext.cs
@@ -16,6 +16,7 @@ namespace HotelAPI.Data.Context
         }
 
         public DbSet<Hotel> Hotel { get; set; }
+        public DbSet<RoomType> RoomType { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
